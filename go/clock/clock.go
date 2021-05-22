1f73ad549418e78ea1249b93ea904e4dbf3db974
Tira Sundara <sundaralinus@gmail.com>
2021-05-22 08:02:59 +0700

    [Go exercise] Improve the clock solution

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -2,16 +2,11 @@ package clock
 
 import (
 	"fmt"
-	"math"
 )
 
 const (
-	hours = 24
-	minutes = 60
-	hLower = 0
-	hUpper = 23
-	mLower = 0
-	mUpper = 59
+	hoursPerday = 24
+	minutesPerHour = 60
 )
 
 // Clock respresents the clock that hold hour, and minute
@@ -21,17 +16,7 @@ type Clock struct {
 
 // New returns Clock type
 func New(h, m int) Clock {
-	if m < mLower {
-		mToh := math.Ceil(math.Abs(float64(m)) / float64(minutes))
-		h = h - int(mToh)
-		m = pmod(m, minutes)
-	}
-
-	div, mod := divmod(m, minutes)
-	_, h = divmod(h + div, hours)
-	m = mod
-
-	return Clock{h, m}
+	return normalize(Clock{h, m})
 }
 
 // String prints the clock in string mode
@@ -49,17 +34,16 @@ func (c Clock) Subtract(m int) Clock {
 	return New(c.h, c.m - m)
 }
 
-func divmod(numerator, denominator int) (quotient, remainder int) {
-	quotient = numerator / denominator
-	remainder = pmod(numerator, denominator)
-	return
-}
+func normalize(c Clock) Clock {
+  c.h += c.m / minutesPerHour
+  if c.m %= minutesPerHour; c.m < 0 {
+  	c.h--
+  	c.m += minutesPerHour
+  }
 
-// Positive modulo, returns non negative solution to x % d  
-func pmod(x, d int) int {  
-  x = x % d
-  if x >= 0 { return x }
-  if d < 0 { return x - d }
-  return x + d
+  if c.h %= hoursPerday; c.h < 0 {
+  	c.h += hoursPerday
+  }
+	return c
 }
 
